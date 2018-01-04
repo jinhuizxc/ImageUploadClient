91398d27c726ec1e2f14815d9b53ba5c00795c2c
jinhuizxc <1004260403@qq.com>
2018-01-04 15:23:59 +0800

    Initial commit

---
+++ app/src/main/java/com/kevin/imageuploadclient/util/Constant.java
@@ -0,0 +1,13 @@
+package com.kevin.imageuploadclient.util;
+
+/**
+ * 服务器地址工具类
+ *
+ * 添加头像
+ * https://app.imerit.cn/tp/index.php/User/usericon_add
+ */
+public class Constant {
+
+    /** 服务器基地址 */
+    public static final String BASE_URL = "http://192.168.10.41:8080/ImageUploadServer";
+}
